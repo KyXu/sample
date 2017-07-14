49af5d0bb48d307869c8ca21d08263a249e8f5cf
KyXu <xukeying@gmail.com>
2017-07-14 17:30:05 -0400

    fff
    first

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var port = process.env.PORT || 3000
+var app = express()
+
+app.set('view', './views')
+app.set('view engine','jade')
+app.listen(port)
+
+console.log('imooc started on port: ' + port)
