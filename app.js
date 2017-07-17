112e094a13bbea71c9059469c6b17f12da567106
KyXu <xukeying@gmail.com>
2017-07-17 12:52:45 -0400

    7.17

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ var express = require('express')
 var port = process.env.PORT || 3000
 var app = express()
 
-app.set('view', './views')
+app.set('views', './views')
 app.set('view engine','jade')
 app.listen(port)
 
 console.log('imooc started on port: ' + port)
+
+//index page
+app.get('/',function(req, res) {
+  res.render('index',{
+    title:'tracking first page'
+  })
+})
+//detail page
+app.get('/movie/:id',function(req, res) {
+  res.render('detail',{
+    title:'tracking detail page'
+  })
+})
+
+app.get('/admin/movie',function(req, res) {
+  res.render('admin',{
+    title:'admin page'
+  })
+})
+
+app.get('/admin/list',function(req, res) {
+  res.render('list',{
+    title:'list'
+  })
+})
